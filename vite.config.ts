c6567f2fdf462f30d76ea1e6988c7dc93fa6568e
Raza Fazal <razafazal19@gmail.com>
2026-05-03 12:28:04 -0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import tailwindcss from "@tailwindcss/vite";
+import viteReact from "@vitejs/plugin-react";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  cloudflare: false,
-  tanstackStart: {
-    // Note: Vercel preset is used automatically by TanStack Start on Vercel
+  plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), nitro(), viteReact()],
+  server: {
+    port: 8080,
+    strictPort: false,
   },
 });
